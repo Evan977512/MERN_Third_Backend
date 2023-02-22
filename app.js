22abee06aff6001df1b5895da6862b785ee4230d
EvanKim <youngchan.kim@edu.sait.ca>
2023-02-22 11:59:26 -0700

    error handling created

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const placesRoutes = require("./routes/places-routes");
+const placesRoutes = require('./routes/places-routes');
 
 const app = express();
 
-app.use("/api/places", placesRoutes); // => /api/places/...만 되도록 설정해보자
+app.use('/api/places', placesRoutes); // => /api/places/...만 되도록 설정해보자
 
-app.listen(5000);
+// create middleware function, default error handler
+// Express will apply on every incoming request
+// 4params is recognized as a special middleware function
+// only executed on the requests that have an error
+app.use((error, req, res, next) => {
+    if (res.headerSent) {
+        return next(error);
+    }
+    // || => if 'error.code' is false it returns 500
+    res.status(error.code || 500);
+    res.json({ message: error.message || 'An unknown error occurred' });
+});
+
+app.listen(5001);
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,27 +1,50 @@
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
 const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire tate Building",
-    description: "One of the most famous  place in the world!",
-    location: { lat: 40.784474, lng: -73.9871516 },
-    address: "wow this is the address New York, NY 10001",
-    creator: "u1",
-  },
+    {
+        id: 'p1',
+        title: 'Empire tate Building',
+        description: 'One of the most famous  place in the world!',
+        location: { lat: 40.784474, lng: -73.9871516 },
+        address: 'wow this is the address New York, NY 10001',
+        creator: 'u1',
+    },
 ];
 
-router.get("/:pid", (req, res, next) => {
-  const placeId = req.params.pid; // { pid: "p1" }
-  // console.log("placeId: ", placeId);
-  const place = DUMMY_PLACES.find((p) => {
-    console.log("p: ", p);
-    return p.id === placeId; // it has to return true
-  });
-  res.json({ place }); // > {place} => {place: place}
-  console.log("placeId: ", placeId);
+router.get('/:pid', (req, res, next) => {
+    const placeId = req.params.pid; // { pid: "p1" }
+    // console.log("placeId: ", placeId);
+    const place = DUMMY_PLACES.find((p) => {
+        console.log('p: ', p);
+        return p.id === placeId; // it has to return true
+    });
+
+    if (!place) {
+        // 404 error response
+        // return res.status(404).json({ message: 'Place not found' });
+        const error = new Error('Could not find a place for the provided pid ' + placeId);
+        error.code = 404;
+        throw error;
+    }
+    res.json({ place }); // > {place} => {place: place}
+    console.log('placeId: ', placeId);
+});
+
+router.get('/user/:uid', (req, res, next) => {
+    const userId = req.params.uid;
+    const place = DUMMY_PLACES.find((p) => {
+        return p.creator === userId;
+    });
+
+    if (!place) {
+        // return res.status(404).json({ message: 'Could not find a place for the provided user id' });
+        const error = new Error('Could not find a place for the provided uid ' + userId);
+        error.code = 404;
+        return next(error); // this will reach the next error handling middleware.
+    }
+    res.json({ place });
 });
 
 // how to link to App.js??
