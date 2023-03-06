f3a7e7f52ee48ecfcc4fb2df8aad138b00ec2ad9
Evan Kim <evan977512@gmail.com>
2023-03-05 19:29:46 -0700

    place schema created

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const placesRoutes = require('./routes/places-routes');
-const usersRoutes = require('./routes/users-routes');
-const HttpError = require('./models/http-error');
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
 app.use(bodyParser.json());
 
 // Create routes
-app.use('/api/places', placesRoutes); // => /api/places/...만 되도록 설정해보자
-app.use('/api/users', usersRoutes);
+app.use("/api/places", placesRoutes); // => /api/places/...만 되도록 설정해보자
+app.use("/api/users", usersRoutes);
 
 // this error handling middleware will be executed if the request is not handled by any of the above middlewares.
 app.use((req, res, next) => {
-    const error = new HttpError('Could not find this route.', 404);
-    throw error; // use 'throw' when it is a synchronous code
+  const error = new HttpError("Could not find this route.", 404);
+  throw error; // use 'throw' when it is a synchronous code
 });
 
 app.use((error, req, res, next) => {
-    if (res.headerSent) {
-        return next(error);
-    }
-    res.status(error.code || 500);
-    res.json({ message: error.message || 'An unknown error occurred!' });
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
 });
 
-app.listen(5001);
+mongoose
+  .connect("mongodb+srv://emdwlekr:rladudcks91@cluster0.wuba1f8.mongodb.net/places?retryWrites=true&w=majority")
+  .then(() => {
+    app.listen(5001);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
