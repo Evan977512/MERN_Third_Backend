f23b0c919873397bf7a6d4c49b5d4ec9bf522cfa
EvanKim <evan977512@gmail.com>
2023-04-10 12:28:06 -0600

    password char number fixed.

--- util/location.js
+++ util/location.js
@@ -2,14 +2,11 @@ const axios = require("axios");
 
 const HttpError = require("../models/http-error");
 
-const API_KEY = "AIzaSyDgLmMpKCzveJf1_yuA0fUzzhy0WRChvZA";
+const API_KEY = "AIzaSyDyzgOvEgjI-9lP2M3OUfSH8Iul6SydXok";
 
+// create function that takes in an address and returns coordinates
 async function getCoordsForAddress(address) {
-  // return {
-  //   lat: 40.7484474,
-  //   lng: -73.9871516
-  // };
-
+  // console.log("address: ", address);
   // axios is a promise based HTTP client for the browser and node.js
   // it is a package for sending Http requests from frontend to backend
   // but we can also use it to send requests from backend to backend or from backend to another bacakend server.
@@ -18,16 +15,16 @@ async function getCoordsForAddress(address) {
   );
 
   const data = response.data;
-  // console.log("DATA: ", data);
+  console.log("DATA: ", data);
 
   // if there is no data or the status is ZERO_RESULTS
   if (!data || data.status === "ZERO_RESULTS") {
     const error = new HttpError("Could not find location for the specified address.", 422);
     throw error;
   }
+
   // returns an object with lat and lng
   const coordinates = data.results[0].geometry.location;
-
   return coordinates;
 }
 
