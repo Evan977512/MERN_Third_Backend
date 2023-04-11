1ca78f0f5fad25feff6f06014e9d9d00cce520eb
EvanKim <evan977512@gmail.com>
2023-04-11 15:05:21 -0600

    signup image upload controller created

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -1,16 +1,17 @@
 const multer = require("multer");
+const uuid = require("uuid");
 
 const MIME_TYPE_MAP = {
-  "image/png": "png,",
-  "image/jpeg": "jpeg,",
-  "image/jpg": "jpg,",
+  "image/png": "png",
+  "image/jpeg": "jpeg",
+  "image/jpg": "jpg",
 };
 
 const fileUpload = multer({
   limits: 500000,
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, "uploads/Images");
+      cb(null, "uploads/images");
     },
     filename: (req, file, cb) => {
       const ext = MIME_TYPE_MAP[file.mimetype];
